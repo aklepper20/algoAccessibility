c927a061380c35519c920ca24335cb5f2a659678
Aly Baez <alybaez@Alys-MBP.home>
2022-03-12 12:25:03 -0800

    format css card

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -31,7 +31,7 @@ function CollectionCard({ name, image, assets, percentAccessibility }) {
         <PriceContainer>
           <img src={marker} alt="Marking Indicator" />
           <Price style={{ color: color }}>
-            Site Inaccessibility: <span>{percentAccessibility}%</span>
+            Inaccessibility: <span>{percentAccessibility}%</span>
           </Price>
         </PriceContainer>
       </Details>
@@ -45,11 +45,13 @@ const Container = styled.div`
   border-radius: 20px;
   overflow: hidden;
   width: 300px;
-  height: 500px;
+  height: 400px;
   margin-right: 30px;
   transition: all 0.5s ease;
   img {
     width: 100%;
+    height: 250px;
+    object-fit: cover;
   }
 `;
 const Details = styled.div`
